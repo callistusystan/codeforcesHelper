32009d665f8fe0cfdb23503ee55d718023a8b6fe
callistusystan <callistusystan@gmail.com>
2017-10-24 05:34:16 +1100

    Enabled setting/resetting template.cpp

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 const cfget = require('../lib/index');
+const argv = require('minimist')(process.argv.slice(2));
 
-const [program, programDirectory, directory, URL] =  process.argv;
-
-cfget.extract(directory, URL);
+if (argv.t) {
+  const path =  argv.t;
+  cfget.setTemplate(path);
+} else if (argv.r) {
+  cfget.resetTemplate();
+} else {
+  const [directory, URL] =  argv._;
+  cfget.extract(directory, URL);
+}
